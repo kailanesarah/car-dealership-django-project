a2f69984e69422bd3485d9ab2d91eea064b49b30
kailanesarah <kailanesarahpro@gmail.com>
2025-03-01 23:35:40 -0300

    Version 01.03.25.23

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,4 @@ urlpatterns = [
     path('logoutAccount/', views.LogoutAccounts, name='LogoutAccounts'),
 
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY_DJANGO') 
+SECRET_KEY =os.getenv("SECRET_KEY")
 #SUA CHAVE DE DESENVOLVIMENTO DE PROJETO (PADRÃO AO SE CRIAR UM NOVO PROJETO DJANGO)
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -128,10 +128,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
